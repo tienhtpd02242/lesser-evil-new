0209b877a60987a11698f4b2d1bb41ca6a406885
vanduy08081998 <vanduy08081998@gmail.com>
2022-11-03 16:31:22 +0700

    fix feedback 3/11

--- package-main/templates/homepage/flavour-section.php
+++ package-main/templates/homepage/flavour-section.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $home_options = get_fields();
 
 $heading_intro = $home_options['heading_fla'];
@@ -9,40 +9,43 @@ $cta_url_fla = $home_options['cta_url_fla'];
 
 $flavours_list = $home_options['flavours_list'];
 
-if ( empty($home_options['show_or_hide_section_fla']) ) {
+if (empty($home_options['show_or_hide_section_fla'])) {
     return;
 }
 ?>
 <section class="flavour-ss">
     <div class="container">
         <div class="wrap">
-            <?php 
+            <?php
 
-            if( !empty($heading_intro) ){ ?>
-                <h2 class="heading"><?php echo $heading_intro;?></h2>
+            if (!empty($heading_intro)) { ?>
+                <h2 class="heading"><?php echo $heading_intro; ?></h2>
             <?php }
 
-            if( !empty($des_inf) ){ ?>
-                <div class="des"><?php echo $des_inf;?></div>
+            if (!empty($des_inf)) { ?>
+                <div class="des"><?php echo $des_inf; ?></div>
             <?php }
 
-            if( !empty($cta_text_fla) && !empty($cta_url_fla) ){ ?>
-                <a class="btn-cm" href="<?php echo $cta_url_fla;?>"><?php echo $cta_text_fla;?></a>
+            if (!empty($cta_text_fla) && !empty($cta_url_fla)) { ?>
+                <a class="btn-cm" href="<?php echo $cta_url_fla; ?>"><?php echo $cta_text_fla; ?></a>
             <?php }
 
-            if ( !empty($flavours_list)) { ?>
+            if (!empty($flavours_list)) { ?>
                 <div class="wrap-fla">
                     <?php foreach ($flavours_list as $value) { ?>
                         <div class="item-fla">
-                            <?php 
-                            if ( !empty( $value['image'])) {
+                            <?php
+                            if (!empty($value['image'])) {
                                 echo "<div class='img'>";
-                                    echo "<img src='". $value['image'] ."' />";
+                                echo "<img src='" . $value['image'] . "' class='can' />";
+                                if (!empty($value['fruit'])) {
+                                    echo "<img src='" . $value['fruit'] . "' class='fruit animated' />";
+                                }
                                 echo '</div>';
-                            } 
+                            }
 
-                            if ( !empty( $value['description'])) { ?>
-                                <div class='des' style='color: <?php echo $value['color'];?>;'><?php echo $value['description'];?></div>
+                            if (!empty($value['description'])) { ?>
+                                <div class='des' style='color: <?php echo $value['color']; ?>;'><?php echo $value['description']; ?></div>
                             <?php }
 
                             ?>
