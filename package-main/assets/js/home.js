a82dd4c8404fcf4940eba9de975c435fb0c19335
vanduy08081998 <vanduy08081998@gmail.com>
2022-11-16 11:16:11 +0700

    fix feedback 16/10

--- package-main/assets/js/home.js
+++ package-main/assets/js/home.js
@@ -1,18 +1,20 @@
-(function( $ ){
+(function ($) {
 
-    $(document).ready( function() {
-        $('.choice-ss .faq_list .item-faq .question').click( function(e) {
+    $(document).ready(function () {
+        $('.choice-ss .faq_list .item-faq .question').click(function (e) {
             e.preventDefault();
 
             $(this).next().slideToggle();
+            $('.choice-ss .faq_list .item-faq').not($(this).parents('.item-faq')).removeClass('active');
+            $('.choice-ss .faq_list .item-faq .answer').not($(this).next()).slideUp();
             $(this).parents('.item-faq').toggleClass('active');
         });
 
-        $('.site-header .__toggle-menu').click( function() {
+        $('.site-header .__toggle-menu').click(function () {
             $(this).toggleClass('active');
             $(this).next().toggleClass('active');
         });
 
     });
 
-})( jQuery );
+})(jQuery);
