54c0c5000e6c658bc422262d9118a19d1de33248
vanduy08081998 <vanduy08081998@gmail.com>
2022-12-05 10:27:46 +0700

    update feedback  05/12

--- package-main/templates/homepage/where-to-buy-section.php
+++ package-main/templates/homepage/where-to-buy-section.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 $home_opts = get_fields();
 
 $heading_gallery = $home_opts['heading_gallery'];
 $des_gallery = $home_opts['description_gallery'];
 
-if ( empty($home_opts['show_or_hide_section_buy'] ) ) {
+if (empty($home_opts['show_or_hide_section_buy'])) {
     return;
-} 
+}
 ?>
 
-<section class="where-to-buy">
+<section class="where-to-buy" id="where-to-buy">
     <div class="container">
-        <?php if( !empty($heading_gallery)) : ?>
-            <h2 class="heading"><?php echo $heading_gallery;?></h2>
+        <?php if (!empty($heading_gallery)) : ?>
+            <h2 class="heading"><?php echo $heading_gallery; ?></h2>
         <?php endif; ?>
 
-        <?php if( !empty($des_gallery)) : ?>
-            <div class="des"><?php echo $des_gallery;?></div>
+        <?php if (!empty($des_gallery)) : ?>
+            <div class="des"><?php echo $des_gallery; ?></div>
         <?php endif; ?>
     </div>
 </section>
